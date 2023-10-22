5c7559def4682f1f328bf391045f44b8fe7c90e2
Sounak008 <sounaks706@gmail.com>
2023-10-22 12:23:00 +0530

    Finished the game

--- javascript.js
+++ javascript.js
@@ -4,4 +4,53 @@ function getComputerChoice(){
     let choiceReturn = choices[randomChoice];
     return (choiceReturn)
 }
-console.log(getComputerChoice());
+function getUserChoice() {
+    let choice = prompt("Enter choice");
+    return (choice);
+    
+}
+
+function gameWinner(computerSelection, playerSelection){
+    if (computerSelection == playerSelection)
+    {
+        return("Its a tie!")
+    }
+    else if (computerSelection === "Rock")
+        {
+            if (playerSelection == "paper")
+            {
+                return("You win! Paper beats rock.")
+            }
+            else
+            {
+                return("You lose! Rock beats paper.")
+            }
+            
+        }
+    else if (computerSelection === "Paper")
+    {
+        if (playerSelection == "scissors")
+            {
+                return("You win! Scissors beat paper.")
+            }
+        else
+            {
+                return("You lose! Paper beats rock.")
+            }
+    }
+    else
+    {
+        if (playerSelection == "rock")
+            {
+                return("You win! Rock beats scissors.")
+            }
+        else
+            {
+                return("You lose! Scissors beat paper.")
+            }
+    }
+}
+
+let computerChoice = getComputerChoice();
+let playerSelection = getUserChoice();
+console.log(gameWinner(computerChoice, playerSelection));
